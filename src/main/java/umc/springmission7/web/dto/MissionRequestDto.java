8d795ab1321ebd17798dc2f8feff9b5d27cf3c41
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-05 14:26:54 +0900

    feat: Create MissionRequestDto.java
    - 보상, 미션내용, 데드라인을 요청으로 받을 Data transfer Object

---
+++ src/main/java/umc/springmission7/web/dto/MissionRequestDto.java
@@ -0,0 +1,18 @@
+package umc.springmission7.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.nio.charset.CodingErrorAction;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MissionRequestDto {
+
+    private Integer reward;
+    private String content;
+    private LocalDate deadline;
+}
