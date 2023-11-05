931a289a88c6c358352737eab56fe9328fb28d89
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-05 14:23:05 +0900

    feat: Create MissionRepository
    - findById 추가

--- src/main/java/umc/springmission7/repository/MissionRepository.java
+++ src/main/java/umc/springmission7/repository/MissionRepository.java
@@ -3,5 +3,8 @@ package umc.springmission7.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.springmission7.domain.Mission;
 
+import java.util.Optional;
+
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+     Optional<Mission> findById(Long id);
 }
