ad5ffae13bd07da1ca2f4ae719f98e96ab7930d3
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-05 14:22:33 +0900

    feat: Update Member.java
    - 변수명 변경: datetime -> deadline
    - List<Review> -> List<MemberMission>으로 연관관계 변경

--- src/main/java/umc/springmission7/domain/Mission.java
+++ src/main/java/umc/springmission7/domain/Mission.java
@@ -2,8 +2,11 @@ package umc.springmission7.domain;
 
 import lombok.*;
 import umc.springmission7.domain.common.BaseEntity;
+import umc.springmission7.domain.enums.MissionStatus;
+import umc.springmission7.domain.mapping.MemberMission;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +21,31 @@ public class Mission extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Integer reward;
 
-    private LocalDate datetime;
+    @Column(nullable = false)
+    private LocalDate deadline;
 
+    //length 의 default = 255
+    @Column(nullable = false)
     private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+    private MissionStatus missionStatus;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="store_id")
     private Store store;
 
     @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
-    private List<Review> memberMissionList = new ArrayList<>();
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+
+    public void updateMissionDetail(Integer reward, String content, LocalDate deadline){
+        this.content = content;
+        this.deadline = deadline;
+        this.reward = reward;
+    }
 
 }
